4cdd09e3a82d7bee9c6041bcc70a7d41bb1634ae
unknown <aferrer48@gmail.com>
2022-02-04 03:14:12 -0300

    ejercicio tarea 2

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const color = require("colors");
-const {mostrarMenu} = require('./helpers/mensajes');
+const { exit } = require("process");
+const {mostrarMenu, pausa} = require('./helpers/mensajes');
 
 console.clear();
 
-const main = () => console.log("Hola Mundo")
+const main = async () => {
+    console.log("Hola Mundo");  
+
+    let opt = '';
+
+    do {
+        opt = await mostrarMenu();
+        if (isNaN(opt) ) {
+            console.log('Debe presionar una numero del 1 a al 5');       
+            
+        }else{
+            console.log({opt});
+        }
+  
+        await pausa();
+
+    } while (opt !== '0' );
+};
 
 main();
 
-mostrarMenu()
+
